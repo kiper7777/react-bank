5616d9ae2d125b2ce0b8b3faea59fcb43e3e40b5
Dmytro Kiperman <8boyss@ukr.net>
2024-08-12 16:58:42 +0200

    upd correct

--- back/src/route/user.js
+++ back/src/route/user.js
@@ -1,85 +1,85 @@
-// // Підключаємо роутер до бек-енду
-// const express = require('express')
-// // створюємо роутер - місце, куди ми підключаємо ендпоїнти
-// const router = express.Router()
+// Підключаємо роутер до бек-енду
+const express = require('express')
+// створюємо роутер - місце, куди ми підключаємо ендпоїнти
+const router = express.Router()
 
-// // Підключіть файли роутів
-// const { User } = require('../class/user')
-// // const {Confirm} = require('../class/confirm')
-// // const {Session} = require('../class/session')
+// Підключіть файли роутів
+const { User } = require('../class/user')
+// const {Confirm} = require('../class/confirm')
+// const {Session} = require('../class/session')
 
-// //=================================
+//=================================
 
-// router.get('/user-list', function (req, res) {
-//     return res.render('user-list', {
-//         name: 'user-list',
-//         component: ['BackButton'],
-//         title: 'User list page',
-//         data: {},
-//     })
-// })
+router.get('/user-list', function (req, res) {
+    return res.render('user-list', {
+        name: 'user-list',
+        component: ['BackButton'],
+        title: 'User list page',
+        data: {},
+    })
+})
 
-// router.get('/user-list-data', function (req, res) {
-//     const list = User.getList()
+router.get('/user-list-data', function (req, res) {
+    const list = User.getList()
 
-//     console.log(list)
+    console.log(list)
 
-//     if (list.length === 0) {
-//         return res.status(400).json({
-//             message: "Список користувачів порожній",
-//         })
-//     }
+    if (list.length === 0) {
+        return res.status(400).json({
+            message: "Список користувачів порожній",
+        })
+    }
 
-//     return res.status(200).json({
-//         list: list.map(({id, email}) => ({
-//             id,
-//             email,
-//         })),
-//     })
-// })
+    return res.status(200).json({
+        list: list.map(({id, email}) => ({
+            id,
+            email,
+        })),
+    })
+})
 
-// //=================================
-// router.get('/user-item', function (req, res) {
-//     return res.render('user-item', {
-//         name: 'user-item',
-//         component: [
-//             'BackButton',
-//             // 'field',
-//             // 'field-password',
-//         ],
+//=================================
+router.get('/user-item', function (req, res) {
+    return res.render('user-item', {
+        name: 'user-item',
+        component: [
+            'BackButton',
+            // 'field',
+            // 'field-password',
+        ],
 
-//         title: 'User item page',
-//         data: {},
-//     })
-// })
+        title: 'User item page',
+        data: {},
+    })
+})
 
-// router.get('/user-item-data', function (req, res) {
-//     const {id} = req.query
-//     // console.log(id)
+router.get('/user-item-data', function (req, res) {
+    const {id} = req.query
+    // console.log(id)
 
-//     if (!id) {
-//         return res.status(400).json({
-//             message: "Потрібно передати ID користувача",
-//         })
-//     }
+    if (!id) {
+        return res.status(400).json({
+            message: "Потрібно передати ID користувача",
+        })
+    }
 
-//     const user = User.getById(Number(id))
+    const user = User.getById(Number(id))
 
-//     if (!user) {
-//         return res.status(400).json({
-//             message: 'Користувач з таким ID не існує',
-//         })
-//     }
+    if (!user) {
+        return res.status(400).json({
+            message: 'Користувач з таким ID не існує',
+        })
+    }
 
-//     return res.status(200).json({
-//         user: {
-//             id: user.id,
-//             email: user.email,
-//             isConfirm: user.isConfirm,
-//         },
-//     })
-// })
+    return res.status(200).json({
+        user: {
+            id: user.id,
+            email: user.email,
+            isConfirm: user.isConfirm,
+        },
+    })
+})
 
-// //=================================
-// // Підключаємо роутер до бек-енду
-// module.exports = router
+//=================================
+// Підключаємо роутер до бек-енду
+module.exports = router
